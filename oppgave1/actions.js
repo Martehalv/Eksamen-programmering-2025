35023ca4464a00848c8406b8f93f5d6bf87d823d
Martehalv <martehal@hiof.no>
2025-11-25 18:43:59 +0100

    jobbet med funksjon 1 og 2

--- oppgave1/actions.js
+++ oppgave1/actions.js
@@ -1,15 +1,15 @@
-// Funksjon 1
-//Funksjon til informasjon om artikler artiklene/kortene
+// Funksjon 1 - Et ansatt kort skal returneres
+// Funksjon til informasjon om artikler artiklene/kortene
 function ansattArtikler(index) {
   const ansatt = ansatte[index];
 
-  //Her måtte det lages en if og loop for kursansvar delen, slik at den får med alle kusene til hver ansatt (Disse er i array i register.js).
+  // Her måtte det lages en if og loop for kursansvar delen, slik at den får med alle kusene til hver ansatt (Disse er i array i register.js).
   let kursInfo = "";
   if (ansatt.kursansvar.length > 0) {
-    //Sjekker arrayet for kurs
+    // Sjekker arrayet for kurs
     kursInfo = ansatt.kursansvar.join(", ");
   } else {
-    //De som ikke har ansvar for noe kurs får dette.
+    // De som ikke har ansvar for noe kurs får dette.
     kursInfo = "Ingen kursansvar";
   }
 
@@ -26,9 +26,18 @@ function ansattArtikler(index) {
   return ansattInfo;
 }
 
-// Her henter jeg inn alle ansatte.
-const alle = document.getElementById("alleAnsatte");
+// Funksjon 2 - Alle ansatt kort skal returneres
 
-for (let i = 0; i < ansatte.length; i++) {
-  alle.innerHTML = alle.innerHTML + ansattArtikler(i);
+function alleAnsatte() {
+  const alle = document.getElementById("");
+  alle.innerHTML = "";
+
+  for (let i = 0; i < ansatte.length; i++) {
+    // Loop som starter på 0, så 1, så plusser den på 1 etter hver gang til det ikke er fler igjen i ansatte arrayet
+    alle.innerHTML = alle.innerHTML + ansattArtikler(i); // Bruker alle ansatte i arrayet (i).
+  }
 }
+
+alleAnsatte(); // Lister ut alle ansatte
+
+// Funksjon 3 - Ta imot og filtrere
