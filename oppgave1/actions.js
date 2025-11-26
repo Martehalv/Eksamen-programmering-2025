43b8fc7098294355c65b84f0adc86d23f5614c29
Martehalv <martehal@hiof.no>
2025-11-26 17:33:37 +0100

    jobbet videre på funksjoner

--- oppgave1/actions.js
+++ oppgave1/actions.js
@@ -27,9 +27,8 @@ function ansattArtikler(index) {
 }
 
 // Funksjon 2 - Alle ansatt kort skal returneres
-
 function alleAnsatte() {
-  const alle = document.getElementById("");
+  const alle = document.getElementById("ansatte-info");
   alle.innerHTML = "";
 
   for (let i = 0; i < ansatte.length; i++) {
@@ -40,4 +39,82 @@ function alleAnsatte() {
 
 alleAnsatte(); // Lister ut alle ansatte
 
-// Funksjon 3 - Ta imot og filtrere
+// Funksjon 3 - Ta imot og filtrere stilling
+function filtrerStilling(stilling) {
+  const alle = document.getElementById("ansatte-info");
+  alle.innerHTML = "";
+
+  // En loop som går igjennom alle ansatte
+  for (let i = 0; i < ansatte.length; i++) {
+    // Sjekker om den ansattes stilling matcher den vi søker etter
+    if (ansatte[i].stilling === stilling) {
+      // Hvis match, legg til kortet
+      alle.innerHTML += ansattArtikler(i);
+    }
+  }
+}
+
+// Funksjon 4 - Lage kurs liste nederst på siden
+function alleKurs() {
+  const kursListe = [];
+
+  // En loop som går gjennom alle ansatte
+  for (let i = 0; i < ansatte.length; i++) {
+    // Looper gjennom kursansvar-arrayet til hver ansatt
+    for (let index = 0; index < ansatte[i].kursansvar.length; index++) {
+      const kurs = ansatte[i].kursansvar[index];
+
+      // Sjekker om kurset allerede er i listen (unngår duplikater)
+      if (!kursListe.includes(kurs)) {
+        kursListe.push(kurs); // Legger til kurset i listen
+      }
+    }
+  }
+
+  // Lager kun <li> elementene
+  let liste = "";
+  for (let i = 0; i < kursListe.length; i++) {
+    liste += `<li>${kursListe[i]}</li>`;
+  }
+
+  // Setter inn kun list items i <ul>
+  document.getElementById("kurs-ul").innerHTML = liste;
+}
+
+alleKurs();
+
+// Funksjon 5 - filtrere undervisere og administrasjon
+function filtrerUndervisere() {
+  const undervisere = document.getElementById(""); // Fyll inn riktig id
+  undervisere.innerHTML = ""; // Tømmer først
+
+  // Looper gjennom alle ansatte
+  for (let i = 0; i < ansatte.length; i++) {
+    // Sjekker om stillingen er Lektor eller Professor
+    if (
+      ansatte[i].stilling === "Lektor" ||
+      ansatte[i].stilling === "Professor"
+    ) {
+      undervisere.innerHTML += ansattArtikler(i);
+    }
+  }
+}
+
+function filtrerAdministrasjon() {
+  const administrasjon = document.getElementById(""); // Fyll inn riktig id
+  administrasjon.innerHTML = ""; // Tømmer først
+
+  // Looper gjennom alle ansatte
+  for (let i = 0; i < ansatte.length; i++) {
+    // Sjekker om stillingen er Rektor, Dekan eller Vaktmester
+    if (
+      ansatte[i].stilling === "Rektor" ||
+      ansatte[i].stilling === "Dekan" ||
+      ansatte[i].stilling === "Vaktmester"
+    ) {
+      administrasjon.innerHTML += ansattArtikler(i);
+    }
+  }
+}
+
+// Funksjon 6 -
